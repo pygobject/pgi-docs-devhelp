7f7e10d28130ee89cdacef92d07134b0b5eb205e
Christoph Reiter <reiter.christoph@gmail.com>
2018-06-16 22:47:04 +0200

    update

--- python-Gegl-0.3/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.3',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt'
-};
